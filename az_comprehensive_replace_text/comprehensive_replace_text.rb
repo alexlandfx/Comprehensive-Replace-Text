8b4fae5d4100d5cf4a760b6b11300598e1224c62
alexlandfx <74614964+alexlandfx@users.noreply.github.com>
2023-09-20 11:19:37 -0700

    Bug fixes, see description
    * Added necessary 'self' to function definition
    * Added nil check on model
    * Added an if statement so non-matching components/materials/tags are not renamed.

--- az_comprehensive_replace_text/comprehensive_replace_text.rb
+++ az_comprehensive_replace_text/comprehensive_replace_text.rb
@@ -6,7 +6,7 @@ require 'sketchup.rb'
 module AlexZahn
   module CRT
 
-    def comprehensiveReplaceText ()
+    def self.comprehensiveReplaceText ()
   
       # Prompt user for text to find
       find_text = UI.inputbox(
@@ -23,46 +23,56 @@ module AlexZahn
       )[0]
   
       model = Sketchup.active_model
-      if model then
-        
-        # BEGIN UNDOABLE OPERATION  
-        model.start_operation('Comprehensive Replace Text', true)
+
+      unless model.nil?
     
         # Get all components in the model
         components = model.definitions.select { |definition| definition.is_a?(Sketchup::ComponentDefinition) }
+
+        ## When the following code is made an operation, component renaming stops working, 
+        ##   and the operation does not appear in the undo menu. Undo menu only shows "Undo Rename Tag".
+        ## BEGIN UNDOABLE OPERATION
+        # model.start_operation('Comprehensive Replace Text', true, false, false)
     
-        # Loop through each component and rename
+        # Loop through each component and rename any matches
         components.each do |component|
-          original_name = component.name
-          new_name = original_name.gsub(find_text, replace_text)
-          component.name = new_name
+          if component.name.include? find_text
+            original_name = component.name
+            new_name = original_name.gsub(find_text, replace_text)
+            component.name = new_name
+          end
         end
     
-        # Loop through each layer and rename
+        # Loop through each layer and rename any matches
         model.layers.each do |layer|
-          original_name = layer.name
-          new_name = original_name.gsub(find_text, replace_text)
-          layer.name = new_name
+          if layer.name.include? find_text
+            original_name = layer.name
+            new_name = original_name.gsub(find_text, replace_text)
+            layer.name = new_name
+          end
         end
     
-        # Loop through each material and rename
+        # Loop through each material and rename any matches
         model.materials.each do |material|
-          original_name = material.name
-          new_name = original_name.gsub(find_text, replace_text)
-          material.name = new_name
+          if material.name.include? find_text
+            original_name = material.name
+            new_name = original_name.gsub(find_text, replace_text)
+            material.name = new_name
+          end
         end
     
         # Rename the model
         model.name = model.name.gsub(find_text, replace_text)
         
-        # END UNDOABLE OPERATION
-        model.commit_operation
+        ## END UNDOABLE OPERATION
+        # model.commit_operation
         
-      end # if
+      end # unless
+
     end # comprehensiveReplaceText
 
     unless file_loaded?(__FILE__)
-      command = UI::Command.new("Comprehensive Replace Text") {comprehensiveReplaceText}
+      command = UI::Command.new("Comprehensive Replace Text") {self.comprehensiveReplaceText}
       command.status_bar_text = "Find-and-Replace text in Components, Tags, Material Names, and Model Name."
       UI.menu('Plugins').add_item(command)
       
